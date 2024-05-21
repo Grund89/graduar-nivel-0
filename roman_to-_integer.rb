d5d1f572d6417eee77837e34e09c81eaf5563db2
Douglas Grund <douglasgrund@yahoo.com.br>
2024-05-21 10:08:14 -0300

    add new method

---
+++ roman_to-_integer.rb
@@ -0,0 +1,16 @@
+# @param {String} s
+# @return {Integer}
+
+def roman_to_int (s)
+  roman_values = {
+    'I' => 1,
+    'V' => 5,
+    'X' => 10,
+    'L' => 50,
+    'C' => 100,
+    'D' => 500,
+    'M' => 1000,
+  }
+
+
+end
