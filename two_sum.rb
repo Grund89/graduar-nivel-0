a37e1dd7048950703f64862111033eedf83f0313
Douglas Grund <douglasgrund@yahoo.com.br>
2024-05-24 14:18:50 -0300

    feat: finished_logic

--- two_sum.rb
+++ two_sum.rb
@@ -7,5 +7,9 @@ def two_suw(nums, target)
  nums.each_with_index do |num, index|
   complement = target - num
 
+  if num_map.key?(complement)
+    return [num_map[complement], index]
+  end
+
 
 end
