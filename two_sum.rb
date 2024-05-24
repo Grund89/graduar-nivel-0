114e0219c1e9f3d4897413b157e335d87f223b31
Douglas Grund <douglasgrund@yahoo.com.br>
2024-05-24 14:28:08 -0300

    fix:incorrectly initialized as an array[] instead of a hash{}

--- two_sum.rb
+++ two_sum.rb
@@ -2,7 +2,7 @@
 # @param {Integer} target
 
 def two_suw(nums, target)
- num_map = []
+ num_map = {}
 
  nums.each_with_index do |num, index|
   complement = target - num
@@ -11,5 +11,15 @@ def two_suw(nums, target)
     return [num_map[complement], index]
   end
 
+  num_map[num] = index
+ end
 
+ []
 end
+
+# testando
+nums = [2, 7, 11, 15]
+target = 9
+result = two_suw(nums, target)
+
+puts "Indices: #{result.inspect}"
